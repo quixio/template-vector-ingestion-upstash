7cd468ee6e4d860b0958cdeae07063e0bc098656
Merlin The Great <merlin+newdemo@quix.io>
2024-05-02 10:55:06 +0000

    Edited 'main.py'

--- Create Embeddings/main.py
+++ Create Embeddings/main.py
@@ -38,6 +38,9 @@ output_topic = app.topic(os.environ['output'], value_serializer="json")
 
 # Initialize a streaming dataframe based on the stream of messages from the input topic:
 sdf = app.dataframe(topic=input_topic)
+
+sdf = sdf.filter(lambda data: data["table"] == "books")
+
 sdf = sdf.update(lambda val: print(f"Original data: {val}"))
 sdf = sdf.apply(simplify_data)
 sdf = sdf.update(lambda val: print(f"Received update: {val}"))
