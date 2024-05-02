4f3af4778584369e19cc371ad70776743698b8e3
Merlin The Great <merlin+newdemo@quix.io>
2024-05-02 10:59:36 +0000

    Edited 'main.py'

--- Create Embeddings/main.py
+++ Create Embeddings/main.py
@@ -26,6 +26,7 @@ def create_embeddings(row):
     return embedding_list
 
 app = Application(
+    consumer_group="vectorsv1",
     auto_offset_reset="earliest",
     auto_create_topics=True,  # Quix app has an option to auto create topics
 )
